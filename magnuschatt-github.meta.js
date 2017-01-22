a4e5f873567957f1a5156dc6d7720377bd2e45f7
magnuschatt <magnus.chatt@gmail.com>
2017-01-22 17:15:36 +0100

    trying out kotlin

---
+++ magnuschatt-github.meta.js
@@ -0,0 +1,1 @@
+// Kotlin.kotlin_module_metadata(5, "magnuschatt-github", "H4sIAAAAAAAAAOPS4xKJd0lNSyzNKQlITM5OTE/Vy84qzhUSkxIRYJBiMmA04hFgluIQYhFiMmA2YOLK41JOgSjXLYCpzy/JycyLLy4pysxLjy9JTMpJFXLX5GLJTczM42KDyHKxhOZllnCxJBalF3OxOhYVJVZysQWDdchwMQkwcrFxMAgwSTCAaRYozSrBIMAIAK2HejOiAAAA");
